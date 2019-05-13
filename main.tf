eaa741f49c6369da39e4ca6c971779b5bbed76c9
“Tim <tim.woodland@clearesult.com>
2019-05-13 09:09:44 -0700

    fixed naming issue

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_sql_database" "db" {
-  name                             = "${local.name}${format("%3d", count.index)}"
+  name                             = "${local.name}${format("%03d", count.index + 1)}"
   count                            = "${var.count}"
   resource_group_name              = "${var.rg_name}"
   location                         = "${var.location}"
